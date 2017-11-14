bcbf5c2df224c5d702911e869ce32931d6cdefbc
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-11-13 22:43:23 -0500

    reducer test makeguess

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -15,6 +15,43 @@ describe ('Reducer', () => {
         expect(state).toBe(currentState);
     });
     describe('newGame', () => {
-        it('Should start a new game', () => )
+        it('Should start a new game', () => {
+            let state = {
+                guesses:[1,2,3,4],
+                feedback: 'Good Guesses',
+                correctAnswer: - 1
+            };
+            state = reducer(state, newGame());
+            expect(state.guesses).toEqual([]);
+            expect(state.feedback).toEqual('Make your guess!');
+            expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
+            expect(state.correctAnswer).toBeLessThanOrEqual(100);
+        });
+    });
+    describe('makeGuess', ()=>{
+        let state = {
+            guesses: [],
+            feedback: '',
+            correctAnswer: 100
+        };
+        state = reducer(state, makeGuess(25));
+        expect(state.guesses).toEqual([25]);
+        expect(state.feedback).toEqual('You\'re Ice Cold...');
+        
+        state = reducer(state, makeGuess(70));
+        expect(state.guesses).toEqual([25,70]);
+        expect(state.feedback).toEqual('You\'re Cold...');
+
+        state = reducer(state, makeGuess(90));
+        expect(state.guesses).toEqual([25,70,90]);
+        expect(state.feedback).toEqual('You\'re Warm');
+
+        state = reducer(state, makeGuess(99));
+        expect(state.guesses).toEqual([25,70,90,99]);
+        expect(state.feedback).toEqual('You\'re Hot!');
+
+        state = reducer(state, makeGuess(100));
+        expect(state.guesses).toEqual([25,70,90,99,100]);
+        expect(state.feedback).toEqual('You got it!');
     })
 })
